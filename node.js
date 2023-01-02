0b67fc4246c05b722e91bd3226b4d269d01e99c5
pollresultsin <121754916+pollresultsin@users.noreply.github.com>
2023-01-02 16:59:56 +0800

    Update node.js

--- node.js
+++ node.js
@@ -1,36 +1,33 @@
-var arrLang = {
-  "en": {
-    "HOME": "Hello!",
-    "ABOUT": "I am poll",
-    "CONTACT": "For testing, to be continued",
-  },
-  "zh": {
-    "HOME": "你好！",
-    "ABOUT": "我叫poll" 
-    "CONTACT": "用于测试，未完待续",
-  }
-};
-// The default language is English
-var lang = "en";
-// Check for localStorage support
-if('localStorage' in window){
-   var lang = localStorage.getItem('lang') || navigator.language.slice(0, 2);
-   if (!Object.keys(arrLang).includes(lang)) lang = 'en';
+$(function(){ 
+      // do something 
+    var script=document.createElement("script");  
+    script.type="text/javascript";  
+    script.src="http://www.microsoftTranslator.com/ajax/v3/WidgetV3.ashx?siteData=ueOIGRSKkd965FeEGM5JtQ**";  
+    document.getElementsByTagName('head')[0].appendChild(script);  
+    var value = sessionStorage.getItem("language");
+    document.onreadystatechange = function () {
+        if (document.readyState == 'complete') {
+            if(value==="1"){
+                Microsoft.Translator.Widget.Translate('zh-CHS', 'en', onProgress, onError, onComplete, onRestoreOriginal, 2000);
+            }
+        }
+    }
+    function onProgress(value) {
+    }
+    function onError(error) {
+    }
+    function onComplete() {
+        $("#WidgetFloaterPanels").hide();
+    }
+    function onRestoreOriginal() { 
+    }
+});
+function translate(){
+    var value = sessionStorage.getItem("language");
+    if(value==="1"){
+        sessionStorage.setItem("language", "0"); 
+    }else{
+        sessionStorage.setItem("language", "1");
+    }
+    window.location.reload();//刷新当前页面.
 }
-$(document).ready(function() {
-  $(".lang").each(function(index, element) {
-    $(this).text(arrLang[lang][$(this).attr("key")]);
-  });
-});
-// get/set the selected language
-$(".translate").click(function() {
-  var lang = $(this).attr("id");
-  // update localStorage key
-  if('localStorage' in window){
-    localStorage.setItem('lang', lang);
-    console.log( localStorage.getItem('lang') );
-  }
-  $(".lang").each(function(index, element) {
-    $(this).text(arrLang[lang][$(this).attr("key")]);
-  });
-});
